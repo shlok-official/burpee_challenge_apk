8d38743e7caacc6e2bb6b7feb36b6200ca14d750
shlokd0619 <shlokdixit@Shloks-MacBook-Pro.local>
2016-01-22 21:04:43 -0500

    01/22/16

---
+++ NUMAD16S-ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit/SecondActivity.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.shlokdixit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shlokdixit on 21/01/16.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        setTitle("Shlok Dixit");
+    }
+}
---
+++ NUMAD16S-ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit/ThirdActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.shlokdixit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shlokdixit on 21/01/16.
+ */
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}

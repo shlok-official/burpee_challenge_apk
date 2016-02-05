ad3e27c9c014d9a4b557418557a92311103e5a7e
shlokd0619 <shlokdixit@Shloks-MBP.cable.rcn.com>
2016-02-05 17:15:00 -0500

    no message

--- ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit/TestDictionary.java
+++ ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit/TestDictionary.java
@@ -100,8 +100,7 @@ adapter.notifyDataSetChanged();
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
                 if (s.length() > 0) {
-                    key = s.toString();
-                    char firstchar = s.charAt(0);
+                   //
                     if (s.length() > 2) {
                         if (firstchar == 'A' || firstchar == 'a') {
 

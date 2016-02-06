b982a4b00a27d3ceafb3c06886bc0a39713b80dd
shlokd0619 <shlokdixit@Shloks-MBP.cable.rcn.com>
2016-02-05 20:45:55 -0500

    no message

--- ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit/testdictionary_acknowledgement.java
+++ ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit/testdictionary_acknowledgement.java
@@ -1,7 +1,18 @@
 package edu.neu.madcourse.shlokdixit;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by shlokdixit on 05/02/16.
  */
-public class testdictionary_acknowledgement {
-}
+public class testdictionary_acknowledgement extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dictionary_acknowledgement);
+
+
+
+}}

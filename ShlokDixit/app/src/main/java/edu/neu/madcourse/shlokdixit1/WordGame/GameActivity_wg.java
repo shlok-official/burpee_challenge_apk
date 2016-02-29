cbc2e4fedea85dae4894ba51aebb17e19440257d
shlokd0619 <shlokdixit@Shloks-MacBook-Pro.local>
2016-02-29 03:19:55 -0500

    no message

--- ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit1/WordGame/GameActivity_wg.java
+++ ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit1/WordGame/GameActivity_wg.java
@@ -23,6 +23,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,7 +40,9 @@ public class GameActivity_wg extends Activity implements CompoundButton.OnChecke
 
     MediaPlayer mMediaPlayer;
     ToggleButton t;
+    CountDownTimer countDownTimer = null;
 
+    TextView timer_wg;
 
 
     @Override
@@ -47,7 +50,7 @@ public class GameActivity_wg extends Activity implements CompoundButton.OnChecke
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_wg_phase_1);
-
+        startCountDownTimer();
         mGameFragment = (GameFragment_wg) getFragmentManager()
                 .findFragmentById(R.id.fragment_game);
 
@@ -60,6 +63,11 @@ public class GameActivity_wg extends Activity implements CompoundButton.OnChecke
         mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.cartoon);
         ///////
         TextView myText = (TextView) findViewById(R.id.phase );
+
+
+        ///////////////
+
+        ///////////////////
 
         Animation anim = new AlphaAnimation(0.0f, 1.0f);
         anim.setDuration(50); //You can manage the blinking time with this parameter
@@ -76,31 +84,59 @@ public class GameActivity_wg extends Activity implements CompoundButton.OnChecke
             if (gameData != null) {
                 // mGameFragment.putState(gameData);
             }
-        }
-        Log.d("UT3", "restore = " + restore);
+        }}
+        //Log.d("UT3", "restore = " + restore);
 /////////////////////////////////////////////////////////////
-        final TextView timer;
+
+
+
+    long total=0;
         ///////////////
-        timer = (TextView) findViewById(R.id.timer);
-        new CountDownTimer(90000, 1000) {
+    private void startCountDownTimer() {
+        timer_wg = (TextView) findViewById(R.id.timer);
+        countDownTimer =
+                new CountDownTimer(90000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                timer.setText("Time Left:00:" + millisUntilFinished / 1000);
+                total = millisUntilFinished;
+                timer_wg.setText("Time Left:00:" + millisUntilFinished / 1000);
 
             }
             public void onFinish() {
-                Toast.makeText(getApplicationContext(), "PHASE-II STARTED", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(GameActivity_wg.this, Phase_II_wd.class);
-                startActivity(intent);
-                finish();
+                //resumeCountDownTimer();
+                //Toast.makeText(getApplicationContext(), "PHASE-II STARTED", Toast.LENGTH_LONG).show();
+                //Intent intent = new Intent(GameActivity_wg.this, Phase_II_wd.class);
+                //startActivity(intent);
+                //finish();
             }
         }.start();
-        //////////////////////////////////////////////////////////
+
+    }
+
+    private void resumeCountDownTimer() {
+        timer_wg = (TextView) findViewById(R.id.timer);
+        countDownTimer =
+                new CountDownTimer(total, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        //total = millisUntilFinished;
+                        timer_wg.setText("Time Left:00:" + millisUntilFinished / 1000);
+
+                    }
+                    public void onFinish() {
+                      //  Toast.makeText(getApplicationContext(), "PHASE-II STARTED", Toast.LENGTH_LONG).show();
+                        //Intent intent = new Intent(GameActivity_wg.this, Phase_II_wd.class);
+                        //startActivity(intent);
+                    //    finish();
+                    }
+                }.start();
 
     }
 
 
 
+//////////////////////////////////////////////////////////
+/*
     public void restartGame() {
         mGameFragment.restartGame();
     }
@@ -137,7 +173,7 @@ public class GameActivity_wg extends Activity implements CompoundButton.OnChecke
 
         // Reset the board to the initial position
         mGameFragment.initGame();
-    }
+    }*/
 /*
     public void startThinking() {
         View thinkView = findViewById(R.id.thinking);
@@ -153,46 +189,53 @@ public class GameActivity_wg extends Activity implements CompoundButton.OnChecke
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
             // The toggle is enabled
-//onPause();
             mMediaPlayer.stop();
             Toast.makeText(getApplicationContext(), "SOUND OFF", Toast.LENGTH_SHORT).show();
 
         } else {
             // The toggle is disabled
-//onResume();
             mMediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.cartoon);
             mMediaPlayer.setLooping(true);
             mMediaPlayer.start();
             Toast.makeText(getApplicationContext(), "SOUND ON", Toast.LENGTH_SHORT).show();
-            //finish();
         }
     }
+public void pausegame(View view){
+    onPause();
 
-    /*
-    boolean isPaused = true;
-    CountDownTimer countDownTimer = null;
+}
 
-   public void pause(View view) {
-        //finish();
-       onPause();
 
-   }
+    /////////////////////////////
+
     @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        if(isPaused==false){
-            countDownTimer.cancel();
-        } else{
-            throw new IllegalStateException("CountDownTimerPausable is already in pause state, start counter before pausing it.");
-        }
-        isPaused = true;
+    public void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), " GAME PAUSED", Toast.LENGTH_SHORT).show();
+        countDownTimer.cancel();
+       }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        //return countDownTimer;
+       // Toast.makeText(getApplicationContext(), " GAME RESUMED", Toast.LENGTH_SHORT).show();
+        //countDownTimer.start();
+        //startCountDownTimer();
+        //countDownTimer.onFinish();
+       //resumeCountDownTimer();
+
+
+
+
     }
-    public boolean isPaused() {
-        return isPaused;
-    }
+
+    /////////////////////////////
 
 
-
+/*
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);  // "Hide" your current Activity

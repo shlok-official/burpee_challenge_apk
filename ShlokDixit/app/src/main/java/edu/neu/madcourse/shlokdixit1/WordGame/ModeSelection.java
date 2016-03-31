924e978f2021658c47ea04b173431c1fd9c77501
shlokd0619 <shlokdixit@Shloks-MacBook-Pro.local>
2016-03-31 02:18:01 -0400

    Mode Selection (single/Double) Player added

---
+++ ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit1/WordGame/ModeSelection.java
@@ -0,0 +1,39 @@
+package edu.neu.madcourse.shlokdixit1.WordGame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import edu.neu.madcourse.shlokdixit1.R;
+import edu.neu.madcourse.shlokdixit1.WordGame.WordGame1Player.Phase_I_wg;
+import edu.neu.madcourse.shlokdixit1.WordGame.WordGame2Player.Phase_I_wg_2Player;
+
+public class ModeSelection extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mode_selection);
+        Button singleplay = (Button)findViewById(R.id.singlePlayerMain);
+        Button multiplay = (Button)findViewById(R.id.twoPlayerMain);
+
+        singleplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ModeSelection.this, Phase_I_wg.class);
+                startActivity(intent);
+            }
+        });
+
+        multiplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ModeSelection.this, Phase_I_wg_2Player.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}

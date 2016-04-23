9706f4c4760fd2ad95a1da2ab760f46324438b32
shlokd0619 <shlokd0619@ccs.neu.edu>
2016-04-22 21:00:43 -0400

    two launcher icons added

---
+++ ShlokDixit/app/src/main/java/edu/neu/madcourse/shlokdixit1/FinalProject/finalproject.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.shlokdixit1.FinalProject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.neu.madcourse.shlokdixit1.R;
+
+public class finalproject extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finalproject);
+
+    }
+
+}
